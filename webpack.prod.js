869074fa16b0cac1cd3f2e458eda99a60828ed49
momo1108 <momo1108@daum.net>
2026-04-24 20:23:40 +0900

    chore(webpack): Webpack 빌드 환경 설정 - 웹팩 설정 파일 분리 : webpack-merge 로 common + dev/prod 구조 적용   - 공통 설정 : webpack.common.js     - entry & output       - entry: src/index.tsx       - output: dist, [name].bundle.js, clean: true     - plugin       - html-webpack-plugin: public/index.html 템플릿 기반 번들 주입       - fork-ts-checker-webpack-plugin: 타입 체크 분리 수행       - dotenv-webpack: APP_PHASE 기반 .env.{phase} 파일 주입     - loader       - html-loader: html 리소스 처리     - module       - asset module: 이미지/폰트 리소스 처리   - 개발 환경 설정 : webpack.dev.js     - mode: development     - sourcemap       - devtool: eval-cheap-module-source-map     - dev server       - host, port, static, hot, liveReload, historyApiFallback 설정       - 프록시 테스트용 설정 추가     - plugin       - @pmmmwh/react-refresh-webpack-plugin: HMR 지원     - loader       - babel-loader: ts/tsx 트랜스파일링 + react-refresh/babel       - css-loader + style-loader: 개발 환경 스타일 처리   - 프로덕션 환경 설정 : webpack.prod.js     - mode: production     - sourcemap       - devtool: false     - plugin       - mini-css-extract-plugin: css 분리 및 파일 생성     - loader       - babel-loader: ts/tsx 트랜스파일링       - mini-css-extract-plugin.loader + css-loader: 배포용 스타일 처리
    - 기존 설정 파일 수정
      - babel.config.json, tsconfig.json
        - react 트랜스파일링의 런타임 옵션을 automatic 으로 적용(Babel이 JSX를 react/jsx-runtime 기반 호출로 변환해서, JSX 사용 시 불필요한 React 기본 import 를 줄임)
      - tsconfig.json
        - types: ["webpack-env"] (리액트 컴포넌트 내에서 환경변수 출력을 위한 타입 정의)
      - package.json
        - scripts 분리: start, build:staging, build:production
        - cross-env로 script 에 NODE_ENV, APP_PHASE 주입
      - .gitignore
        - 환경 변수 파일을 제외
    
    - 의존성 설치
      - webpack, webpack-cli, webpack-dev-server, webpack-merge
      - babel-loader, style-loader, css-loader, html-loader
      - html-webpack-plugin, fork-ts-checker-webpack-plugin
      - @pmmmwh/react-refresh-webpack-plugin, react-refresh
      - mini-css-extract-plugin, dotenv-webpack, @types/webpack-env

---
+++ webpack.prod.js
@@ -0,0 +1,29 @@
+const { merge } = require('webpack-merge');
+const common  = require('./webpack.common.js');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = merge(common, {
+    mode: 'production',
+    devtool: false,
+    plugins: [
+        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/i,
+                loader: 'babel-loader',
+                exclude: ['/node_modules/'],
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            },
+        ]
+    }
+})
