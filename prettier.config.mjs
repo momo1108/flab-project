b4bf7b518d0358a2e30269682a1774c7e7b89941
momo1108 <momo1108@daum.net>
2026-04-25 19:13:58 +0900

    chore(lint): husky 기반 eslint-prettier pre-commit 파이프라인 구성 - eslint 설정(eslint.config.mjs)   - ignores 패턴으로 dist, node_modules 파일 제외   - Flat Config 기반 설정 적용 : JS, TS, React 기본 설정   - 필요없는 규칙 비활성화   - 웹팩 설정 파일을 cjs 로 처리되도록 별도 처리 - prettier 설정(prettier.config.mjs) : 포맷 규칙 정리 - husky 설정(.husky/pre-commit) : pre-commit 훅에서 lint-staged 실행 연결 - lint-staged 설정(lint-staged.config.mjs) : lint-staged에 prettier write, eslint fix 작업 등록 - package.json   - scripts - prepare : 의존성 설치 시 husky install 자동 실행 적용   - 의존성 설치 : eslint, prettier, eslint-plugin-prettier, eslint-config-prettier, eslint-plugin-react, typescript-eslint, globals, husky, lint-staged 추가

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  endOfLine: 'auto', // 첫번째 라인의 방식으로 모든 라인에 적용
+};
+
+export default config;
