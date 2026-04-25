704577b1229c9c5b6acee1c59eb317ead2a8a25c
momo1108 <momo1108@daum.net>
2026-04-25 19:15:30 +0900

    refactor: 기존 소스코드와 설정 파일에 린터를 적용

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,13 +1,10 @@
 import './styles.css';
 import { type ButtonHTMLAttributes } from 'react';
 
-
 type ButtonProps = { children: React.ReactNode } & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = (props: ButtonProps) => {
-    return <button {...props}>
-        { props.children } 버튼
-    </button>
-}
+  return <button {...props}>{props.children} 버튼</button>;
+};
 
 export default Button;
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,50 +1,44 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
-const common  = require('./webpack.common.js');
+const common = require('./webpack.common.js');
 
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'eval-cheap-module-source-map', // 빠른 빌드와 디버깅을 위한 소스맵 설정
-    devServer: {
-        open: true,
-        host: 'localhost',
-        port: 3000,
-        static: path.resolve(__dirname, 'public'),  // 정적 파일을 제공할 디렉토리
-        liveReload: true,    // 라이브 리로딩 활성화 (기본값: true)
-        hot: true,           // 핫 모듈 교체 활성화
-        historyApiFallback: true, // 히스토리 API를 사용하는 SPA에 유용
-        proxy: [
-            {
-                context: ['/google'],
-                target: 'https://www.google.com',
-                changeOrigin: true,
-                pathRewrite: { '^/google': '' },
-            },
-        ]
-    },
-    plugins: [
-        new ReactRefreshWebpackPlugin(),
+  mode: 'development',
+  devtool: 'eval-cheap-module-source-map', // 빠른 빌드와 디버깅을 위한 소스맵 설정
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 3000,
+    static: path.resolve(__dirname, 'public'), // 정적 파일을 제공할 디렉토리
+    liveReload: true, // 라이브 리로딩 활성화 (기본값: true)
+    hot: true, // 핫 모듈 교체 활성화
+    historyApiFallback: true, // 히스토리 API를 사용하는 SPA에 유용
+    proxy: [
+      {
+        context: ['/google'],
+        target: 'https://www.google.com',
+        changeOrigin: true,
+        pathRewrite: { '^/google': '' },
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/i,
-                loader: 'babel-loader',
-                exclude: ['/node_modules/'],
-                options: {
-                    plugins: [require.resolve('react-refresh/babel')]
-                }
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    "style-loader" ,
-                    "css-loader"
-                ]
-            },
-        ]
-    }
-})
+  },
+  plugins: [new ReactRefreshWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: 'babel-loader',
+        exclude: ['/node_modules/'],
+        options: {
+          plugins: [require.resolve('react-refresh/babel')],
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+});
